05b0e47c83fc1beb3390385ef5a64fc14b4f4a20
MarilenaCretu <marilenac2001@gmail.com>
2022-12-02 19:11:13 +0200

    Initial commit

---
+++ src/test/java/WishList/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        WishlistTest wishlistTest=new WishlistTest();
+       // wishlistTest.addToWishlistTest();//cautare produs=search
+        wishlistTest.addToWishlistSale();
+    }
+}
